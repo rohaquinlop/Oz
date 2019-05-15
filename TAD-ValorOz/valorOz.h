970587d0e1ee873ea5ca8d37d8d02f0d69c604ad
fockdislaif <rohaquinlop301@gmail.com>
2019-05-15 08:55:01 -0500

    Cambio en el comentario

--- TAD-ValorOz/valorOz.h
+++ TAD-ValorOz/valorOz.h
@@ -14,7 +14,8 @@ class ValorOz{
         //Por medio de herencia se deben de crear los demas valores Oz que faltan
         //cada uno seria de la siguiente manera:
         //      ValorOzUnlinked, ValorOzInt, ValorOzFloat, ValorOzVar, ValorOzRec.
-        //Nota: Implementar los registros, un registro es un mapa de strins, hacia valorOz, de esta manera se logra la "recursividad" para que un registro pueda tener sub-registros
+        //Nota: Implementar los registros, un registro es un mapa de strings, hacia valorOz
+        //de esta manera se logra la "recursividad" para que un registro pueda tener sub-registros
 
     public:
         //Constructor, metodos: set, get
