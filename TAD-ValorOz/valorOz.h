55981e93ba35285df9434d6353dc26e0eed0b2e3
fockdislaif <rohaquinlop301@gmail.com>
2019-05-15 13:20:52 -0500

    Añadidas las clases hijas de la clase ValorOz Se crearon las clases hijas las cuales se usan para establecer el tipo de ValorOz que van a ser, int, float, var, rec

--- TAD-ValorOz/valorOz.h
+++ TAD-ValorOz/valorOz.h
@@ -4,6 +4,7 @@
 //Importar las librerias necesarias
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -20,8 +21,60 @@ class ValorOz{
     public:
         //Constructor, metodos: set, get
         ValorOz(string _type);
-        setType(string _type);
+        void setType(string _type);
         string getType();
+};
+
+class ValorOzUnlinked : public ValorOz{
+    private:
+        //Atributo de la clase
+        string val;
+    public:
+        //Constructor de la clase, metodos set y get.
+        ValorOzUnlinked(string _val);
+};
+
+class ValorOzInt : public ValorOz{
+    private:
+        int val;
+    public:
+        //Constructor de la clase, metodos set y get.
+        ValorOzInt(int _val);
+        void setVal(int _val);
+        int getVal();
+};
+
+class ValorOzFloat : public ValorOz{
+    private:
+        double val;
+    public:
+        //Constructor de la clase, metodos set y get.
+        ValorOzFloat(double _val);
+        void setVal(double _val);
+        double getVal();
+};
+
+class ValorOzVar : public ValorOz{
+    private:
+        string var;
+    public:
+        //Constructor de la clase, metodos set y get
+        ValorOzVar(string _var);
+        void setVal(string _var);
+        string getVal();
+};
+
+class ValorOzRec : public ValorOz{
+    private:
+        string name;
+        map<string, ValorOz> m;
+    public:
+        //Constructor de la clase, metodos set y get.
+        ValorOzRec(string _name, map<string, ValorOz> _m);
+        void setName(string _name);
+        void setMap(map<string, ValorOz> _m);
+        string getName();
+        map<string, ValorOz> getMap();
 };
 
 #endif
