adea54613de9ff732cecd468819f06829e0404d2
fockdislaif <rohaquinlop301@gmail.com>
2019-05-16 14:30:50 -0500

    Cambios en las librerias Se añadieron las librerias de cada ValorOz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "Operaciones/operaciones.h"
 #include "TAD-Almacen/almacen.h"
 #include "TAD-ValorOz/valorOz.h"
+#include "TAD-ValorOz/valorOzFloat.h"
+#include "TAD-ValorOz/valorOzInt.h"
+#include "TAD-ValorOz/valorOzVar.h"
+#include "TAD-ValorOz/valorOzRec.h"
+#include "TAD-ValorOz/valorOzUnlinked.h"
 #include <iostream>
 #include <string>
 
