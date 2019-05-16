cb645218fb3c8dfcaf641460ee73577136ec551c
fockdislaif <rohaquinlop301@gmail.com>
2019-05-16 15:09:35 -0500

    Comentarios funcion Parse

--- Operaciones/operaciones.cpp
+++ Operaciones/operaciones.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 //Métodos, set, get, constructores...
 map<string, string> Operaciones :: parse(string _s){
+	/*
+	Operacion que evalua un string que ingresa, y determina
+	cada campo que hay en el string.
+	Ejemplo:
+		- X=10 |--> c1 = X y c2 = 10
+	*/
 	map<string, string> fields;
 	string c1 = "", c2 = "";
 	int i;
 
 	for(i = 0; i < _s.size(); i++){
-		if( _s[i] == ' ' ){
+		if( _s[i] == '=' ){
 			i++;
 			break;
 		}else{
